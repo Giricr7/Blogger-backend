719a39c7e7fb69f64e9d1a643c88c4a30f173e12
Giriprasath <giriarjun1995@gmail.com>
2022-03-03 15:21:08 -0700

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:process.env.CLIENT_URL
+}));
 app.use("/posts", require("./routes/postRoutes"));
 app.use("/user", require("./routes/authRoute"));
 
