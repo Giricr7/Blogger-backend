31b00a12f8999a4a28099f4c0b3f439030df7956
Giriprasath <giriarjun1995@gmail.com>
2022-03-03 16:57:07 -0700

    first commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://mypersonalblogger.netlify.app/',
+  origin: 'https://mypersonalblogger.netlify.app',
   credentials:true,     
   optionSuccessStatus:200
 }));
